7a6c175cd186a5df8c93686d1001164bd9ec4f20
Turuall <turusin@mail.ru>
2022-11-02 12:05:12 +0800

    задача 52

---
+++ lesson7/task52/Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+
+
+Console.WriteLine("сколько строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сколько столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] twoDimArray = new int[m, n];
+Random rnd = new Random();
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        { Console.Write($"{matr[i, j]} "); }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        { matr[i, j] = Convert.ToInt32(rnd.Next(0, 100)); }
+    }
+}
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray);
+double[] b = new double[n];
+for (int j = 0, k = 0; j < n; j++)
+{
+    {
+        double summ = 0;
+        for (int i = 0; i < m; i++)
+
+        { summ += twoDimArray[i, j]; }
+        b[k] = summ / m;
+        Console.WriteLine("Среднее столбца " + (j + 1) + " = " + Math.Round(b[k], 2));
+        k++;
+    }
+}
